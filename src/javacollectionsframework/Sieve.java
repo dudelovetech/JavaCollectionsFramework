2a505520ba6f64dfe6f2d1d7ccff6ad2ab8b207d
dudelovetech <ds.sun.lau@gmail.com>
2018-04-30 00:40:09 -0700

    initilization

---
+++ src/javacollectionsframework/Sieve.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacollectionsframework;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author donny
+ */
+public class Sieve {
+    public static void main(String[] args) {
+        //display all numbers to be processed
+        // TODO: call Sieve method       
+    }
+    
+    private static List<String> Sieve(int maxNum){
+        List<Integer> numbersToProcess = new LinkedList<>();
+        //TODO: return linkedlist
+    }
+}
