a4d5ca4ba6779b46b8a06b997c65e3c496d03ce7
dudelovetech <ds.sun.lau@gmail.com>
2018-05-17 22:19:31 -0700

    ...

---
+++ src/Maps/WordCount.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author donny
+ */
+public class WordCount {
+    
+    public static void main(String[] args) {
+        Map<String, Double> salaryMap = new HashMap<String, Double>();
+    }
+}
