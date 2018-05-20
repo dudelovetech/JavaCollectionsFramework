cc56b21a36c4c450833900425c3c6312dd8ababe
dudelovetech <ds.sun.lau@gmail.com>
2018-05-19 23:25:17 -0700

    finished

--- src/Maps/WordCount.java
+++ src/Maps/WordCount.java
@@ -5,11 +5,14 @@
  */
 package Maps;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  *
@@ -17,29 +20,42 @@ import java.util.Set;
  */
 public class WordCount {
 
-    public static void main(String[] args) {
-        Map<Integer, String> ssnMap = new HashMap<>();
-
-        ssnMap.put(867530915, "Jenny");
-        ssnMap.put(239876305, "Bin");
-        ssnMap.put(504386382, "Donny");
-
-        Set<Integer> ssnSet = ssnMap.keySet();
-        for (int ssn : ssnSet) {
-            System.out.println("SSN: " + ssn);
+    // minimum number of occurrences needed to be printed
+    public static final int OCCURRENCES = 100;
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("This program displays the most");
+        System.out.println("frequently occurring words from");
+        System.out.println("the book Moby Dick.");
+        System.out.println();
+        
+        // read the book into a map
+        Scanner in = new Scanner(new File("C:\\Users\\donny\\Documents\\NetBeansProjects\\JavaCollectionsFramework\\src\\Maps\\mobydick.txt"));
+        Map<String, Integer> wordCountMap = getCountMap(in);
+        
+        for (String word: wordCountMap.keySet()) {
+            int count = wordCountMap.get(word);
+            if (count > OCCURRENCES) {
+                System.out.println(word + " occurs " + count + " times.");
+            }
         }
+    }
 
-        for (Map.Entry<Integer, String> entry : ssnMap.entrySet()) {
-            Integer key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println("SSN: " + key + "/" + "name: " + value);
+    // Reads book text and returns a map from words to counts.
+    private static Map<String, Integer> getCountMap(Scanner in) {
+        Map<String, Integer> wordCountMap = new TreeMap<>();
+        
+        while(in.hasNext()) {
+            String word = in.next().toLowerCase();
+            if (!wordCountMap.containsKey(word)) {
+                // never seen this word before
+                wordCountMap.put(word, 1);
+            } else {
+                // seen this word before; increment count
+                int count = wordCountMap.get(word);
+                wordCountMap.put(word, count + 1);
+            }
         }
-
-        Collection<String> names = ssnMap.values();
-        for (String name : names) {
-            System.out.println("name: " + name);
-        }
-
-        System.out.println(ssnMap);
+        return wordCountMap;
     }
 }
